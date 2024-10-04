36a32b2936ae08fbf9e2341b1296983987706c9c
Canuski <ojalexander24@gmail.com>
2024-10-04 20:20:58 +0200

    Checkpoint

--- Assets/Scripts/VideoLoopManager.cs
+++ Assets/Scripts/VideoLoopManager.cs
@@ -4,28 +4,42 @@ using UnityEngine.Video;
 public class VideoLoopManager : MonoBehaviour
 {
     // References to the video players
-    public VideoPlayer videoEnterOnePlay;   // First video: entering the elevator
-    public VideoPlayer videoFadeIntoElevator;  // Second video: fade into elevator
-    public VideoPlayer videoStandLooped;    // Third video: standing in the elevator (looped)
-    public VideoPlayer videoFadeOut;        // Final video: fade out (end)
+    public VideoPlayer videoEnterOnePlay;       // First video: entering the elevator
+    public VideoPlayer videoFadeIntoElevator;   // Second video: fade into elevator
+    public VideoPlayer videoStandLooped;        // Third video: standing in the elevator (looped)
+    public VideoPlayer videoFadeOut;            // Final video: fade out (end)
+
+    public GameObject buttonsPanel; // UI Panel containing the buttons
 
     // New videos for the sequence after the fade-out
-    public VideoPlayer videoExtraOne;    // First video after fade-out
-    public VideoPlayer videoExtraTwo;    // Second video after fade-out
+    public VideoPlayer videoExtraOne;           // First video after fade-out
+    public VideoPlayer videoExtraTwo;           // Second video after fade-out
+
+    // Videos for the "Floor 2" sequence
+    public VideoPlayer videoEnterFloorTwo;      // First video for Floor 2: entering the elevator
+    public VideoPlayer videoFadeIntoFloorTwo;   // Fade into elevator video for Floor 2
 
     // GameObjects for controlling visibility
-    public GameObject videoEnterOnePlayObject;  // GameObject for Video-Enter-OnePlay
+    public GameObject videoEnterOnePlayObject;      // GameObject for Video-Enter-OnePlay
     public GameObject videoFadeIntoElevatorObject;  // GameObject for Fade into elevator video
-    public GameObject videoStandLoopedObject;   // GameObject for standing looped video
-    public GameObject videoFadeOutObject;       // GameObject for Fade-out video
-    public GameObject videoExtraOneObject;      // GameObject for the first extra video
-    public GameObject videoExtraTwoObject;      // GameObject for the second extra video
+    public GameObject videoStandLoopedObject;       // GameObject for standing looped video
+    public GameObject videoFadeOutObject;           // GameObject for Fade-out video
+    public GameObject videoExtraOneObject;          // GameObject for the first extra video
+    public GameObject videoExtraTwoObject;          // GameObject for the second extra video
+
+    // GameObjects for Floor 2 sequence
+    public GameObject videoEnterFloorTwoObject;     // GameObject for entering elevator (Floor 2)
+    public GameObject videoFadeIntoFloorTwoObject;  // GameObject for fade into elevator (Floor 2)
+
 
     private bool videoEndCalled = false;  // To track if the ending sequence is called
     private bool inExtraSequence = false; // To track if we are in the extra video sequence
+    private bool floorTwoSelected = false; // To track if the Floor 2 sequence is selected
 
     void Start()
     {
+        buttonsPanel.SetActive(false);
+
         // Subscribe to the video finished events
         videoEnterOnePlay.loopPointReached += OnEnterOnePlayFinished;
         videoFadeIntoElevator.loopPointReached += OnFadeIntoElevatorFinished;
@@ -33,6 +47,10 @@ public class VideoLoopManager : MonoBehaviour
         videoFadeOut.loopPointReached += OnFadeOutFinished;
         videoExtraOne.loopPointReached += OnExtraOneFinished;
         videoExtraTwo.loopPointReached += OnExtraTwoFinished;
+
+        // Subscribe to Floor 2 video events
+        videoEnterFloorTwo.loopPointReached += OnEnterFloorTwoFinished;
+        videoFadeIntoFloorTwo.loopPointReached += OnFadeIntoFloorTwoFinished;
     }
 
     // First video finished (Entering the elevator)
@@ -56,6 +74,7 @@ public class VideoLoopManager : MonoBehaviour
         videoStandLoopedObject.SetActive(true);
         videoStandLooped.isLooping = true;
         videoStandLooped.Play();
+        buttonsPanel.SetActive(true);
     }
 
     // Loop video reached (this happens repeatedly due to looping)
@@ -74,6 +93,8 @@ public class VideoLoopManager : MonoBehaviour
         // Stop the looped video
         videoStandLooped.Stop();
         videoStandLoopedObject.SetActive(false);
+
+        buttonsPanel.SetActive(false);
 
         // Activate the fade-out video
         videoFadeOutObject.SetActive(true);
@@ -121,19 +142,51 @@ public class VideoLoopManager : MonoBehaviour
         RestartLoopedSequence();
     }
 
+    // Handle Floor 2 button press
+    public void StartFloorTwoSequence()
+    {
+        floorTwoSelected = true;
+
+        // Deactivate the looped video and start the Floor 2 sequence
+        videoStandLooped.Stop();
+        videoStandLoopedObject.SetActive(false);
+
+        videoEnterFloorTwoObject.SetActive(true);
+        videoEnterFloorTwo.Play();
+    }
+
+    // First Floor 2 video finished (Entering the elevator)
+    void OnEnterFloorTwoFinished(VideoPlayer vp)
+    {
+        videoEnterFloorTwoObject.SetActive(false);
+
+        videoFadeIntoFloorTwoObject.SetActive(true);
+        videoFadeIntoFloorTwo.Play();
+    }
+
+    // Fade into Floor 2 finished
+    void OnFadeIntoFloorTwoFinished(VideoPlayer vp)
+    {
+        videoFadeIntoFloorTwoObject.SetActive(false);
+
+    }
+
     // Return to the looped fade-in/fade-out sequence
     void RestartLoopedSequence()
     {
         inExtraSequence = false;
+        floorTwoSelected = false;
 
-        // Activate Fade-into-elevator video (like resetting the sequence)
+        // Reset the sequence by returning to the standing-in-elevator loop
         videoFadeIntoElevatorObject.SetActive(true);
         videoFadeIntoElevator.Play();
+
+        buttonsPanel.SetActive(true);
     }
 
     public void StartSequence()
     {
-        // Start the first video sequence
+        // Start the first video sequence (default)
         videoEnterOnePlayObject.SetActive(true);
         videoEnterOnePlay.Play();
     }
@@ -147,5 +200,9 @@ public class VideoLoopManager : MonoBehaviour
         videoFadeOut.loopPointReached -= OnFadeOutFinished;
         videoExtraOne.loopPointReached -= OnExtraOneFinished;
         videoExtraTwo.loopPointReached -= OnExtraTwoFinished;
+
+        // Clean up Floor 2 video events
+        videoEnterFloorTwo.loopPointReached -= OnEnterFloorTwoFinished;
+        videoFadeIntoFloorTwo.loopPointReached -= OnFadeIntoFloorTwoFinished;
     }
 }
